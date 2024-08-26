e3ecfcee895e663d0025f414e7393c6d3676608f
JQ <livejiaquan010313@gamil.com>
2024-08-26 12:17:31 +0800

    update JQ.py

--- JQ.py
+++ JQ.py
@@ -1,16 +1,14 @@
-
 from ultralytics import YOLO
 
-# model = YOLO(r'D:\YOLOv8_data\YOLO_tools\yolov8-streamlit-detection-tracking\weights\0719_ppl_car_model_L.pt')
-model = YOLO(r'D:\YOLOv8_data\YOLO_tools\yolov8-streamlit-detection-tracking\weights\0730_ppe.pt')
+model = YOLO(r'C:\Users\limjiaquan\AppData\Local\anaconda3\envs\YOLOv8env\Lib\site-packages\ultralytics\yolov8n.pt')
 
-source = r'D:\YOLOv8_data\CAM13-images_part_1'
-
-model.predict( source, 
+rtsp_url = 'URL'
+for result in model.predict( source = rtsp_url, 
                save= True, 
                imgsz= 640, 
                conf= 0.5,
-            #    classes = 0,  # 僅檢測person
+               classes = 0,  # 僅檢測person
                save_txt= True,   # 保存符合Darklabel的YOLO txt格式
                save_frames= False,# 關閉即時串流視頻時同時輸出圖像和標籤
-               )
+               ):
+    pass
